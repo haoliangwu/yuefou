ac8fa3ded0bba170775a12763d61c6a83d8f606e
Lyon <wuhaoliang@cloudist.cc>
2018-03-27 10:41:17 +0800

    [ADD] 增加 pm2 ecosystem 配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,48 @@
+require('dotenv').config({ path: './.env' })
+
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'yuefou-graphql-server',
+      script: 'dist/index.js',
+      "env_webhook": {
+        "port": 23928,
+        "path": "/webhook",
+        "secret": "yuefouweb"
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  // deploy : {
+  //   production : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/production',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  //   },
+  //   dev : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/development',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+  //     env  : {
+  //       NODE_ENV: 'dev'
+  //     }
+  //   }
+  // }
+};
