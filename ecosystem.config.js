40c428efa94365302c734288571c077a88c901bb
Lyon <wuhaoliang@cloudist.cc>
2018-03-27 13:48:03 +0800

    [TRIVIAL] last test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: './.env' })
 
 module.exports = {
+  
   /**
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
