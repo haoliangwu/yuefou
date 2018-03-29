7651838e06a26cf1a5bde13934e15c7a57a80483
Lyon <wuhaoliang@cloudist.cc>
2018-03-29 15:39:15 +0800

    [IMP] 配置 pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,5 +20,20 @@ module.exports = {
         NODE_ENV: 'production'
       }
     }
-  ]
+  ],
+  deploy: {
+    "staging": {
+      "key": "~/.ssh/id_rsa_1",
+      "user": "ubuntu",
+      "host": "58.87.91.173",
+      "ref": "origin/staging",
+      "repo": "git@github.com:haoliangwu/yuefou.git",
+      "path": "/home/ubuntu/yuefou",
+      "ssh_options": ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
+      "post-deploy": "pm2 startOrRestart ecosystem.config.js --env dev",
+      "env": {
+        "NODE_ENV": "staging"
+      }
+    }
+  }
 };
--- sync.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash 
-
-rsync -avzP --delete ./dist ./tmp ubuntu@prisma.littlelyon.com:/home/ubuntu/lyon/yuefou/
