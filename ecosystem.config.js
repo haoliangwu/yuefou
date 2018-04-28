ae2cd866b3ea96550e082b416575c9e88804e530
Lyon <wuhaoliang@cloudist.cc>
2018-04-28 17:56:47 +0800

    [IMP] SF

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       "host": "58.87.91.173",
       "ref": "origin/master",
       "repo": "git@github.com:haoliangwu/yuefou.git",
-      "path": "/home/ubuntu/yuefou",
+      "path": "/home/ubuntu/release/yuefou",
       "ssh_options": ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
       "post-deploy": "yarn install && yarn prod:deploy",
       "env": {
@@ -38,7 +38,7 @@ module.exports = {
       "host": "58.87.91.173",
       "ref": "origin/staging",
       "repo": "git@github.com:haoliangwu/yuefou.git",
-      "path": "/home/ubuntu/release/yuefou",
+      "path": "/home/ubuntu/yuefou",
       "ssh_options": ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
       "post-deploy": "yarn install && yarn prod:deploy",
       "env": {
